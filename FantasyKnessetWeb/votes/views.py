509e3e0af4f58204c7ca90e747b04ab552b5b6bb
oded golden <odedgolden@gmail.com>
2013-12-26 16:01:08 +0200

    'views an db- oded'

--- FantasyKnessetWeb/votes/views.py
+++ FantasyKnessetWeb/votes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponseNotAllowed
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
@@ -9,13 +10,13 @@ from votes.models import Candidate
 class IndexView(generic.ListView):
     template_name = 'votes/index.html'
     context_object_name = 'candidate_list'
-
     def get_queryset(self):
         return Candidate.objects.all()[:20]   
      
 class voting(generic.ListView):
     template_name = 'votes/voting.html'
     context_object_name = 'candidate_list'
+    voted = 'true'
     
     def get_queryset(self):
         return Candidate.objects.all()[:20]   
