1019bd272435809803043cadb69f0a434e938eb8
Tom Gurion <nagasaki45@gmail.com>
2013-12-29 09:50:54 +0200

    Update model
    Remove duplication in wikipedia url.
    
    Remove "numb_of_votes".
    
    Change "users" to "voters". This field is also used to calculate
    what previously called "num_of_votes".

--- votes/models.py
+++ votes/models.py
@@ -2,17 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
 class Candidate(models.Model):
     name = models.CharField(max_length=200)
     pesonal_site = models.URLField(null=True, blank=True)
     facebook_page = models.URLField(null=True, blank=True)
     wikpedia_article = models.URLField(null=True, blank=True)
-    wikpedia_url = models.URLField(null=True, blank=True)
     image_url = models.URLField(null=True, blank=True)
-    num_of_votes = models.IntegerField(default=0)
+    voters = models.ManyToManyField(User, null=True, blank=True)
 
-    users = models.ManyToManyField(User, null=True, blank=True)
-    
-    def __unicode__(self):  
+    def __unicode__(self):
         return self.name
