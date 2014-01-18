7f755bc56905d1dff8db439c6d36b79e078ec405
Tom Gurion <nagasaki45@gmail.com>
2014-01-18 21:44:01 +0200

    New log on every vote up or vote down. Logs registered for admin panel

--- votes/admin.py
+++ votes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from votes.models import Candidate
+from votes.models import Candidate, Log
 
 # Register your models here.
 admin.site.register(Candidate)
+admin.site.register(Log)
