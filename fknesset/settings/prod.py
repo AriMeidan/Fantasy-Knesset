074aeeeeed4bc3dee27940dae7e20863af91e40b
Ari Meidan <meidan.swe@gmail.com>
2014-01-23 15:36:11 +0200

    splited settings file to prod and dev

---
+++ fknesset/settings/prod.py
@@ -0,0 +1,15 @@
+from base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+CSRF_COOKIE_SECURE = True
+
+
+SESSION_COOKIE_SECURE = True
+
+
+ALLOWED_HOSTS = ['*']
+
