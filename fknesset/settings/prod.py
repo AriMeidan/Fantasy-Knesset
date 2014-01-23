aae4fc0e6c20442135320529837029c3661fd683
Ari Meidan <meidan.swe@gmail.com>
2014-01-23 19:39:09 +0200

    Still working on prod settings file...

--- fknesset/settings/prod.py
+++ fknesset/settings/prod.py
@@ -26,7 +26,3 @@ ALLOWED_HOSTS = ['*']
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
