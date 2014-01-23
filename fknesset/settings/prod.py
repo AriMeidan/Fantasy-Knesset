89de5180f5d874f4332c9cf44803aae518e198bd
Ari Meidan <meidan.swe@gmail.com>
2014-01-23 19:20:14 +0200

    commented out STATICFILES_DIR in prod settings file

--- fknesset/settings/prod.py
+++ fknesset/settings/prod.py
@@ -26,6 +26,6 @@ ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
