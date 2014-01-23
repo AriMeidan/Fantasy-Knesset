e98905e1deb50390bdb3796b0ab755b8d9c8888c
Ari Meidan <meidan.swe@gmail.com>
2014-01-23 18:22:05 +0200

    moved DATABASES to dev settings file

--- fknesset/settings/base.py
+++ fknesset/settings/base.py
@@ -57,19 +57,6 @@ ROOT_URLCONF = 'fknesset.urls'
 WSGI_APPLICATION = 'fknesset.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE':'django.db.backends.postgresql_psycopg2',
-        'NAME': 'fknesset',
-        'USER': 'su',
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    },
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
--- fknesset/settings/dev.py
+++ fknesset/settings/dev.py
@@ -1,1 +1,15 @@
 from base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fknesset',
+        'USER': 'su',
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    },
+}
