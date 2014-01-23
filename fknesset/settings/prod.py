75f2fff1becb27d5c05658461275511fc8422da1
Ari Meidan <meidan.swe@gmail.com>
2014-01-23 20:15:28 +0200

    Still working on prod settings file...

--- fknesset/settings/prod.py
+++ fknesset/settings/prod.py
@@ -32,7 +32,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    
+    os.path.join(BASE_DIR, 'votes', 'static')
 )
 
 
