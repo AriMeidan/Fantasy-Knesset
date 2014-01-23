ee0670a103aa77616a4087fc894ef7a0f27e9742
Tom Gurion <nagasaki45@gmail.com>
2014-01-23 23:19:18 +0200

    Still working on prod settings file...

--- fknesset/settings/prod.py
+++ fknesset/settings/prod.py
@@ -23,11 +23,4 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
