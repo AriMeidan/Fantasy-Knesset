04bff712d2eb574a44bfad7f3099d2d5afb11c82
Tom Gurion <nagasaki45@gmail.com>
2014-01-29 11:31:02 +0200

    Register the Party model in the admin site

--- votes/admin.py
+++ votes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from votes.models import Candidate, Log
+from votes.models import Party, Candidate, Log
 
 # Register your models here.
+admin.site.register(Party)
 admin.site.register(Candidate)
 admin.site.register(Log)
