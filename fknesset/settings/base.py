34c368e8a31d7441225a294629f885181052df13
Tom Gurion <nagasaki45@gmail.com>
2014-02-05 19:53:48 +0200

    Implement safe way to get env variable in settings files

--- fknesset/settings/base.py
+++ fknesset/settings/base.py
@@ -8,6 +8,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+def get_environ(key, critical=True):
+    '''
+    Safe way to get environment variable.
+    Recommendation from "Two Scoops" (p. 39).
+    '''
+
+    try:
+        return os.environ[key]
+    except Exception, e:
+        if critical:
+            raise ImproperlyConfigured('Set the {}'
+                ' environment variable'.format(key))
+        return None
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
@@ -17,7 +32,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = get_environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
