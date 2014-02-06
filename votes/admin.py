ab15ae90a2945f7b821e298b89c4538855791d12
Tom Gurion <nagasaki45@gmail.com>
2014-02-06 12:20:24 +0200

    Adding lots of user admin goodies :-)

--- votes/admin.py
+++ votes/admin.py
@@ -1,13 +1,61 @@
 from django.contrib import admin
 from votes.models import Party, Candidate, Log
+from django.contrib.auth.models import Group
 
-# Register your models here.
+#----------------------------------------------------------
+#   VOTES MODELS ADMIN
+#----------------------------------------------------------
+
 admin.site.register(Party)
 admin.site.register(Candidate)
 admin.site.register(Log)
 
-# adding costum user admin
+
+#----------------------------------------------------------
+#   CUSTOM USER ADMIN
+#----------------------------------------------------------
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(get_user_model(), UserAdmin)
+# module scooped moderator group
+moderators_group, created = Group.objects.get_or_create(name='moderators')
+
+
+def make_moderator(modeladmin, request, queryset):
+    '''
+    Custom admin action - add user to the group "moderators".
+    '''
+
+    for user in queryset.all():
+        user.groups.add(moderators_group)
+
+
+def disable_moderator(modeladmin, request, queryset):
+    '''
+    Custom admin action - remove user from the group "moderators".
+    '''
+
+    for user in queryset.all():
+        user.groups.remove(moderators_group)
+
+
+class MyUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('moderator',)
+    actions = [make_moderator, disable_moderator]
+
+
+    def moderator(self, user):
+        '''
+        Returns whether the user is moderator.
+        '''
+        
+        if moderators_group in user.groups.all():
+            return True
+        else:
+            return False
+
+    moderator.boolean = True
+
+
+admin.site.register(get_user_model(), MyUserAdmin)
